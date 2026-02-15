909bb372bb7a5c934c23d990af29e89f48da0a33
Ian Njenga <bnjeynga2@gmail.com>
2026-02-14 22:48:05 -0500

    Fix Hilt compatibility: Kotlin 2.2.0 + KSP 2.2.0-2.0.2

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,8 +2,8 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
